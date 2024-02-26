cba6363f04bb91352534f1652c9642438c27ae50
a23310173 <a23310173@ceti.mx>
2024-02-26 07:24:43 -0600

    Cambios de errores para hacer merge a main

--- cmake-build-debug/CMakeFiles/rpg.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/rpg.dir/DependInfo.cmake
@@ -8,10 +8,10 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "E:/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Character/Character.cpp" "CMakeFiles/rpg.dir/Character/Character.cpp.obj" "gcc" "CMakeFiles/rpg.dir/Character/Character.cpp.obj.d"
-  "E:/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Enemy/Enemy.cpp" "CMakeFiles/rpg.dir/Enemy/Enemy.cpp.obj" "gcc" "CMakeFiles/rpg.dir/Enemy/Enemy.cpp.obj.d"
-  "E:/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Player/Player.cpp" "CMakeFiles/rpg.dir/Player/Player.cpp.obj" "gcc" "CMakeFiles/rpg.dir/Player/Player.cpp.obj.d"
-  "E:/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/main.cpp" "CMakeFiles/rpg.dir/main.cpp.obj" "gcc" "CMakeFiles/rpg.dir/main.cpp.obj.d"
+  "/Volumes/ELITE SE880/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Character/Character.cpp" "CMakeFiles/rpg.dir/Character/Character.cpp.o" "gcc" "CMakeFiles/rpg.dir/Character/Character.cpp.o.d"
+  "/Volumes/ELITE SE880/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Enemy/Enemy.cpp" "CMakeFiles/rpg.dir/Enemy/Enemy.cpp.o" "gcc" "CMakeFiles/rpg.dir/Enemy/Enemy.cpp.o.d"
+  "/Volumes/ELITE SE880/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/Player/Player.cpp" "CMakeFiles/rpg.dir/Player/Player.cpp.o" "gcc" "CMakeFiles/rpg.dir/Player/Player.cpp.o.d"
+  "/Volumes/ELITE SE880/Almacenamiento/C++/P.O.O/Practica_3_Juego/RPG_2P_23310173/main.cpp" "CMakeFiles/rpg.dir/main.cpp.o" "gcc" "CMakeFiles/rpg.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
